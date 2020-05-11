4f5c3cae2c8b68ca7b7ec162e165fc219353d3f2
Myles <mylosadler@gmail.com>
2020-05-11 22:12:01 +0100

    init.

---
+++ blog/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+# Create your models here.
+class Blog(models.Model):
+    title = models.CharField(max_length=200)
+    pub_date = models.DateTimeField(auto_now_add=True)
+    body = models.CharField(max_length=5000)
+    blogimage = models.ImageField(upload_to='images/')
+
+    def __str__(self):
+        return self.title
+
+    def summary(self):
+        return self.body[:100]
+
+    def pub_date_pretty(self):
+        return self.pub_date.strftime('%b %e %Y')
---
+++ jobs/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+from .models import Job
+
+def home(request):
+    jobs = Job.objects
+    return render(request, 'home.html',{'jobs':jobs})
+
+# Create your views here.
